6a93355d56e8879ab01d57670b9fad56f8936216
Gustavo Morales <gus.morales@me.com>
2020-08-06 23:20:26 -0400

    fixing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
