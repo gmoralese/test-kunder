151e94a9eabd24d6de199159d60335df7b8b0c1b
Gustavo Morales <gus.morales@me.com>
2020-08-06 22:34:53 -0400

    otra vez

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/app'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/kunder-exam-frontend'));
+
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+    res.sendFile(path.join(__dirname + '/dist/kunder-exam-frontend/index.html'));
 });
