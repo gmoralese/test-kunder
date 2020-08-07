7c1e1b24f461cffb2033b3e8439f1db694a6975a
Gustavo Morales <gus.morales@me.com>
2020-08-07 12:37:18 -0400

    asd

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
     res.sendFile(path.join(__dirname + '/dist/kunder-exam-frontend/app.component.html'));
 });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
