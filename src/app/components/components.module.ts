5f927aa5a9d7e8fc4b6cf2b19444ec4f98e465e2
Gustavo Morales <gus.morales@me.com>
2020-08-06 23:04:18 -0400

    fixing

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent],
+  declarations: [BannerComponent],
   imports: [],
   exports: [HomeComponent, BannerComponent]})
 
